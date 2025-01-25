4a6f38450d2b1791a832556f19eb604ebfefbc39
Syed Asif <aftabsyedasif@gmail.com>
2025-01-26 02:17:35 +0530

    Write send ping and transfer programs

---
+++ generate-keypair/transfer.ts
@@ -0,0 +1,62 @@
+import {
+    Connection,
+    Transaction,
+    SystemProgram,
+    sendAndConfirmTransaction,
+    PublicKey,
+} from "@solana/web3.js";
+import "dotenv/config";
+import { getKeypairFromEnvironment } from "@solana-developers/helpers";
+
+const suppliedToPubkey = process.argv[2] || null;
+
+if (!suppliedToPubkey) {
+    console.log(`Please provide a public key to send to`);
+    process.exit(1);
+}
+
+const senderKeypair = getKeypairFromEnvironment("SECRET_KEY");
+
+console.log(`senderKeypair: ${senderKeypair.publicKey.toBase58()}`);
+
+console.log(`suppliedToPubkey: ${suppliedToPubkey}`);
+
+const toPubkey = new PublicKey(suppliedToPubkey);
+
+const connection = new Connection("https://api.devnet.solana.com", "confirmed");
+
+console.log(
+    `✅ Loaded our own keypair, the destination public key, and connected to Solana`,
+);
+
+const balance = await connection.getBalance(senderKeypair.publicKey);
+
+console.log(`Balance: ${balance}`);
+
+const toBalance = await connection.getBalance(toPubkey);
+
+console.log(`To Balance: ${toBalance}`);
+
+const transaction = new Transaction();
+
+const LAMPORTS_TO_SEND = 5000;
+
+const sendSolInstruction = SystemProgram.transfer({
+    fromPubkey: senderKeypair.publicKey,
+    toPubkey,
+    lamports: LAMPORTS_TO_SEND,
+});
+
+transaction.add(sendSolInstruction);
+
+const signature = await sendAndConfirmTransaction(connection, transaction, [
+    senderKeypair,
+]);
+
+const toBalanceAfter = await connection.getBalance(toPubkey);
+
+console.log(`To Balance After: ${toBalanceAfter}`);
+console.log(
+    `💸 Finished! Sent ${LAMPORTS_TO_SEND} to the address ${toPubkey}. `,
+);
+console.log(`Transaction signature is ${signature}!`);
